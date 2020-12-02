f8fb659455680891ed17aa8bedaae81d77237ee3
TRAMPENAU Rene <Rene.Trampenau@akka.eu>
2020-12-02 13:07:12 +0100

    added part b for door 1 and 2

--- door1.py
+++ door1.py
@@ -2,8 +2,18 @@ arr = []
 with open('door1.txt') as f:
     for line in f:
         arr.append(int(line.strip('\n')))
-    
+
+# part A
 for i in range(len(arr)):
-    for j in arr[i:]:
+    for j in arr[i+1:]:
         if ((arr[i]+j) == 2020):
-            print(arr[i]*j)
+            print(f'Part A: {arr[i]*j}')
+
+# part B
+for i in range(len(arr)):
+    for j in range(len(arr[i:])):
+        for k in arr[j:]:
+            if ((arr[i]+arr[j]+k) == 2020):
+                print(f'Part B: {arr[i]*arr[j]*k}')
+                
+              
--- door2.py
+++ door2.py
@@ -1,15 +1,28 @@
-count = 0
+partA = 0
+partB = 0
 
 with open('door2.txt','r') as f:
     for line in f:
+        # part A
+
         rule = line.split(':')[0]
         password = line.split(':')[1]
         letter = rule[-1]
         quantity = rule.split()[0]
         quantity_low = quantity.split('-')[0]
         quantity_high = quantity.split('-')[1]
-        
+
         if (password.count(letter) >= int(quantity_low)) and (password.count(letter) <= int(quantity_high)):
-            count += 1
-            
-    print(count)
+            partA += 1
+
+        # part B
+
+        first_pos = int(quantity_low)
+        second_pos = int(quantity_high)
+
+        if ((password[first_pos] == letter) and  (password[second_pos] != letter)) or ((password[first_pos] != letter) and  (password[second_pos] == letter)):
+            partB += 1
+
+
+    print(f'Part A: {partA}')
+    print(f'Part B: {partB}')
