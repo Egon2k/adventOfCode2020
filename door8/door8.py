b2025f229df37c38198a35048030bb824ec25fb0
TRAMPENAU Rene <Rene.Trampenau@akka.eu>
2020-12-08 08:30:01 +0100

    added door8 part2

--- door8/door8.py
+++ door8/door8.py
@@ -1,5 +1,18 @@
+import pprint
+
 pc = 0
 acc = 0
+
+def replaceInstr(mem,n):
+    count = 0
+    for line in mem:
+        #if 'nop' in line:
+        if 'jmp' in line:
+            if count == n:
+                #line.replace('nop','jmp')
+                line.replace('jmp','nop')
+            count += 1
+    return mem
 
 if __name__ == "__main__":
     data = []
@@ -27,6 +40,45 @@ if __name__ == "__main__":
             elif instr == 'jmp':
                 pc += val
 
-    print(acc)
+    print(f'Part1 {acc}')
+
+    # part 2
+    for i in range(len(data)):
+        mem = data[:]
+        pc = 0
+        acc = 0
+        alreadyExecuted = [0] * len(mem)
+
+        if "jmp" in mem[i]:
+            mem[i] = mem[i].replace("jmp", "nop")
+        elif "nop" in mem[i]:
+            mem[i] = mem[i].replace("nop", "jmp")
+        else:
+            continue
+
+        try:
+            while(0 <= pc <= len(mem) and alreadyExecuted[pc] == 0):
+                instr = mem[pc].split()[0].replace(' ','')
+                val   = int(mem[pc].split()[1].replace(' ',''))
+
+                if alreadyExecuted[pc] == 1 :
+                    break
+                else:
+                    alreadyExecuted[pc] = 1
+
+                    if instr == 'nop':
+                        pc += 1
+                    elif instr == 'acc':
+                        pc += 1
+                        acc += val
+                    elif instr == 'jmp':
+                        pc += val
+
+            if pc >= len(mem):
+                break
+        except:
+            break
+
+    print(f'Part2 {acc}')
 
 
