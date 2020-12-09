aa76a01a808fcedb64233c65040f4f3c727fc66f
TRAMPENAU Rene <Rene.Trampenau@akka.eu>
2020-12-09 08:24:18 +0100

    added door9 part2

--- door9/door9.py
+++ door9/door9.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     with open('data.txt','r') as f:
         data = f.read()[:-1].split('\n') # [:-1] to cut off the last \n
-    
+
     numbers = list(map(int, data))
 
     # part 1
@@ -17,3 +17,12 @@ if __name__ == "__main__":
             if not anyequalto(numbers[i-25:i], number):
                 print(f'Part1: {number}')
                 break
+
+    # part 2
+    for i in range(len(numbers)):
+        # exclude the number itself, otherwise it would find two results
+        if not numbers[i] == number:
+            for j in range(len(numbers)-i):
+                seg = numbers[i:i+j]
+                if sum(seg) == number:
+                    print(f'Part2: {min(seg) + max(seg)}')
