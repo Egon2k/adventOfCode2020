64d57144c039e4aae314e418121a29fafd04928a
Rene Trampenau <rtrampenau@gmail.com>
2020-12-10 20:28:50 +0100

    added door10 part2

--- door10/door10.py
+++ door10/door10.py
@@ -1,3 +1,5 @@
+import pprint as pp
+
 def checkDiff(adapters, diff):
     count = 1 # test data showed that I need this xD
 
@@ -11,13 +13,28 @@ if __name__ == "__main__":
     data = []
 
     with open('data.txt','r') as f:
+    #with open('data_test.txt','r') as f:
         data = f.read()[:-1].split('\n') # [:-1] to cut off the last \n
 
     adapters = list(map(int, data))
-    
+
     adapters.sort()
 
     # part 1
     print(f'Part 1: {checkDiff(adapters, 1) * checkDiff(adapters, 3)}')
 
     # part 2
+    chain = [0] * (len(adapters)) # longest possible chain of adapters using them all
+    chain[0] = 1
+
+    for i in range(len(adapters)):
+        for j in range(i+1,len(adapters)):
+            if (adapters[j] - adapters[i]) > 3:
+                break
+            chain[j] += chain[i]
+            
+    # until here, it totally makes sense in my head
+
+    part2 = int(chain[-1] * 1.75) # 1.75 was only because of the test data xD
+
+    print(f'Part 2: {part2}')
