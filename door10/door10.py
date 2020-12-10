140bccc88ec9912264c280f13a4a0b50abeb23b3
TRAMPENAU Rene <Rene.Trampenau@akka.eu>
2020-12-10 07:40:29 +0100

    added door10 part1

---
+++ door10/door10.py
@@ -0,0 +1,21 @@
+def checkDiff(adapters, diff):
+    adapters.sort()
+
+    count = 1 # test data showed that I need this xD
+
+    for i in range(len(adapters)):
+        if (adapters[i] - adapters[i-1]) == diff:
+            count += 1
+
+    return count
+
+if __name__ == "__main__":
+    data = []
+
+    with open('data.txt','r') as f:
+        data = f.read()[:-1].split('\n') # [:-1] to cut off the last \n
+
+    adapters = list(map(int, data))
+
+    # part 1
+    print(f'Part1 : {checkDiff(adapters, 1) * checkDiff(adapters, 3)}')
