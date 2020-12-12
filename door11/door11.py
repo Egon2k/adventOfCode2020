96c260782962f48959da92b63d425d437380c787
Rene Trampenau <rtrampenau@gmail.com>
2020-12-12 09:10:36 +0100

    added door11 part2

--- door11/door11.py
+++ door11/door11.py
@@ -1,4 +1,4 @@
-def checkNeighbors(data, row, seat, lastRow, lastSeat):
+def checkDirectNeighbors(data, row, seat, lastRow, lastSeat):
     neighbors = 0
     fromRow = 0
     toRow = 0
@@ -27,7 +27,6 @@ def checkNeighbors(data, row, seat, lastRow, lastSeat):
 
     temp = data[fromRow:toRow]
 
-
     for line in range(len(temp)):
         neighbors += temp[line][fromSeat:toSeat].count('#')
 
@@ -36,6 +35,34 @@ def checkNeighbors(data, row, seat, lastRow, lastSeat):
 
     return neighbors
 
+def checkNeighbors(data, row, seat, lastRow, lastSeat):
+    neighbors = 0
+
+    directions = [(-1,-1), #up left
+                  (-1, 0), #up
+                  (-1, 1), #up right
+                  ( 0, 1), #right
+                  ( 1, 1), #down right
+                  ( 1, 0), #down
+                  ( 1,-1), #down left
+                  ( 0,-1), #left
+                  ]
+    
+    for direction in directions:
+        for i in range(1,min(lastRow,lastSeat)):
+            drow  = i * direction[0]
+            dseat = i * direction[1]
+            
+            if (0 <= (row + drow) < lastRow) and (0 <= (seat + dseat) < lastSeat):
+                if data[row + drow][seat + dseat] == 'L':
+                    break
+                elif data[row + drow][seat + dseat] == '#':
+                    neighbors += 1
+                    break
+                elif data[row + drow][seat + dseat] == '.':
+                    continue
+            
+    return neighbors
 
 if __name__ == "__main__":
     with open('data.txt','r') as f:
@@ -43,21 +70,53 @@ if __name__ == "__main__":
 
     for i in range(len(data)):
         data[i] = list(data[i])
-
+        
+    
     rows = len(data)
     seats = len(data[0])
+
+    # part 1
 
     # https://stackoverflow.com/a/28684323
     before = [x[:] for x in data]
     after  = [x[:] for x in data]
-
+    
     changed = True
-
+    
     while changed:
         changed = False
-
+    
         before = [x[:] for x in after]
+    
+        for i in range(rows):
+            for j in range(seats):
+                if before[i][j] == '.':
+                    continue
+                if before[i][j] == 'L' and checkDirectNeighbors(before, i, j, rows, seats) == 0:
+                    after[i][j] = '#'
+                    changed = True
+                if before[i][j] == '#' and checkDirectNeighbors(before, i, j, rows, seats) >= 4:
+                    after[i][j] = 'L'
+                    changed = True
+        print('.', end='')
+    
+    part1 = 0
+    for i in after:
+        part1 += i.count('#')
+    
+    print(f'\nPart 1: {part1}')
 
+    # part 2
+    before = [x[:] for x in data]
+    after  = [x[:] for x in data]
+    
+    changed = True
+    
+    while changed:
+        changed = False
+    
+        before = [x[:] for x in after]
+    
         for i in range(rows):
             for j in range(seats):
                 if before[i][j] == '.':
@@ -65,13 +124,12 @@ if __name__ == "__main__":
                 if before[i][j] == 'L' and checkNeighbors(before, i, j, rows, seats) == 0:
                     after[i][j] = '#'
                     changed = True
-                if before[i][j] == '#' and checkNeighbors(before, i, j, rows, seats) >= 4:
+                if before[i][j] == '#' and checkNeighbors(before, i, j, rows, seats) >= 5:
                     after[i][j] = 'L'
                     changed = True
-        print('.', end='')
-
-    part1 = 0
+    
+    part2 = 0
     for i in after:
-        part1 += i.count('#')
-
-    print(f'\nPart 1: {part1}')
+        part2 += i.count('#')
+    
+    print(f'\nPart 2: {part2}')
